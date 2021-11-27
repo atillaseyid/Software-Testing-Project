c964d4a46a577ef24cd94706253ec68cfae8b080
atillaseyid <atillaseyid87@gmail.com>
2021-11-27 16:55:32 +0100

    Contact Us Step definitions added

---
+++ src/test/java/ContactUsStepDefs.java
@@ -0,0 +1,38 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.Optional;
+
+import static org.junit.Assert.fail;
+
+public class ContactUsStepDefs extends AbstractStepDefs{
+
+    @Given("Contact Us Link clicked")
+    public void ContactUsLinkClicked(){
+        HomePage.GetContactUsLink().click();
+    }
+    @And("Contact Us {string} is filled with {string}")
+    public void ContactUsFieldFilled(String field, String parameter){
+        HomePage.FillField(field, parameter);
+    }
+
+    @When("Send button clicked")
+    public void SendButtonClicked(){
+        HomePage.GetSendButton().click();
+    }
+    @Then("{string} contact us error message is shown")
+    public void ContactUsErrorMsgShown(String msg){
+        Optional<String> ErrorMessage = HomePage.GetContactError();
+        if( ErrorMessage.isPresent())
+            Assert.assertEquals(msg,ErrorMessage.get());
+        else
+            fail();
+    }
+
+
+
+
+}
