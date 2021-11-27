6baf79d17a8b1912346b10c7699f2145ce45f722
atillaseyid <atillaseyid87@gmail.com>
2021-11-27 16:23:52 +0100

    Search Step Definitions created and added.

---
+++ src/test/java/SearchStepDefs.java
@@ -0,0 +1,39 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.Optional;
+
+import static org.junit.Assert.fail;
+
+public class SearchStepDefs extends AbstractStepDefs {
+
+    @Given("Search {string} filled with {string}")
+    public void SearchFieldFilled(String field,String parameter){
+        HomePage.FillField(field, parameter);
+    }
+    @When("Search Button clicked")
+    public void CartLinkSelected(){
+        HomePage.GetSearchButton().click();
+    }
+    @Then("{string} search error msg shown")
+    public void SearchErrorMessageIsShown(String msg){
+        Optional<String> ErrorMessage = HomePage.GetSearchError();
+        if(ErrorMessage.isPresent())
+            Assert.assertEquals(msg,ErrorMessage.get());
+        else
+            fail();
+    }
+    @Then("{string} search result message shown")
+    public void ResultFoundMsgShown(String result_msg){
+        Optional<String> ResultMessage = HomePage.GetSearchResult();
+        if(ResultMessage.isPresent()){
+            Assert.assertEquals(result_msg, ResultMessage.get());
+        }
+        else
+            fail();
+    }
+
+
+}
