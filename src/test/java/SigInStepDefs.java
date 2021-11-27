2d2ea3732760527063702d3ac62c4282cc2a49d6
atillaseyid <atillaseyid87@gmail.com>
2021-11-27 14:46:01 +0100

    SignIn Step Definitions has been created and committed

---
+++ src/test/java/SigInStepDefs.java
@@ -0,0 +1,33 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.Optional;
+
+import static org.junit.Assert.fail;
+
+public class SigInStepDefs extends AbstractStepDefs{
+
+    @Given("{string is filled with {string}")
+    public void FieldIsFilledWithParameter(String field,String parameter){
+        HomePage.FillField(field,parameter);
+    }
+    @When("SignIn button is clicked")
+    public void SignInButtonIsClicked(){
+        HomePage.GetSignInButton().click();
+    }
+    @And("SignIn link is clicked")
+    public void SignInLinkIsClicked(){
+        HomePage.GetSignInLink().click();
+    }
+    @Then("{string} error msg is shown")
+    public void ErrorMessageIsShown(String msg){
+        Optional<String> ErrorMessage = HomePage.GetLoginError();
+        if (ErrorMessage.isPresent())
+            Assert.assertEquals(msg,ErrorMessage.get());
+        else
+            fail();
+    }
+}
